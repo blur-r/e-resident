b9a659d7b1bf0df0291ff4965bbda5f8c9543c74
Babalola Paul <pbabalola27@gmail.com>
2025-07-28 11:03:05 +0100

    Add initial components and styles for navigation, pages, and layout
    - Created FeaturedPpt4SaleOrRent component.
    - Added Navbar component for navigation.
    - Initialized pages: Checkout, Pdp, Profile, Signin, Signup with empty files.
    - Updated main.scss to include all page styles.
    - Created empty SCSS files for buy, checkout, rent, shortlet, signin, and signup pages.
    - Added basic styling for home page header.

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+
+    return (
+        <div className="nav">
+
+
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import bgImg from "../assets/images/bgImg.png"
+
+export default function Home() {
+    return (
+        <>
+            <h1>Welcome!</h1>
+            <img src={bgImg} alt="" />
+        </>
+    )
+}
