f30def39a126a6eafbf70bae7a0b20aed048a256
Babalola Paul <pbabalola27@gmail.com>
2025-08-03 22:02:11 +0100

    Add Shortlet page with responsive layout and styles; update routing in App component; refactor PropertyCard to use className

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Rent from "./pages/Rent";
 import Buy from "./pages/Buy";
+import Shortlet from "./pages/Shortlet";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/rent" element={<Rent />} />
                 <Route path="/buy" element={<Buy />} />
+                <Route path="/shortlet" element={<Shortlet />} />
             </Routes>
 
         </>
--- src/components/PptCard.jsx
+++ src/components/PptCard.jsx
@@ -1,14 +1,14 @@
 function PropertyCard() {
     return (
-        <div class="property-card">
-            <div class="image-container">
+        <div className="property-card">
+            <div className="image-container">
                 <img src="/images/bgImg.png" alt="Luxury 3 Bedroom Detached Duplex" />
-                <div class="image-overlay">
-                    <button class="nav-button left">&lt;</button>
-                    <button class="nav-button right">&gt;</button>
+                <div className="image-overlay">
+                    <button className="nav-button left">&lt;</button>
+                    <button className="nav-button right">&gt;</button>
                 </div>
             </div>
-            <div class="details">
+            <div className="details">
                 <h3>Luxury 3 Bedroom Detached Duplex</h3>
                 <p className="rent">Rent: N1,500,000/Annum</p>
                 <p className="location">Location: Lagos</p>
